a92d565a4d138663c09deba0448d5b989c15cee5
schoeneberg <schoeneberg@physik.rwth-aachen.de>
2022-03-11 13:27:59 +0100

    Made the module installation more robust, now is actual installation instead of moving some modules

--- liquidcosmo/__init__.py
+++ liquidcosmo/__init__.py
@@ -1,5 +1,5 @@
-from src.folder import folder
-from src.matplotlib_defaults import matplotlib_defaults
+from .folder import folder
+from .matplotlib_defaults import matplotlib_defaults
 matplotlib_defaults()
 def load(path):
   return folder.load(path)
