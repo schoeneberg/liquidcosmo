c5669cd5f75090a5583d1a3fbead8a93a4bfa569
schoeneberg <schoeneberg@github.com>
2023-03-02 15:03:08 +0100

    Small bugfix related to **kwargs vs kwargs

--- liquidcosmo/__init__.py
+++ liquidcosmo/__init__.py
@@ -4,7 +4,7 @@ from .folder import folder
 from .foldercollection import foldercollection
 def load(*args,**kwargs):
   if len(args)==1 and isinstance(args[0],str):
-    return folder.load(args[0],kwargs)
+    return folder.load(args[0],**kwargs)
   else:
     return foldercollection.load(*args,**kwargs)
 def load_chain(path,**kwargs):
