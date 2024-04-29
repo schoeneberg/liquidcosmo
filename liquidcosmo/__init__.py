6d3dea3553d246d63ecbb3500d8d3910b9b2059b
schoeneberg <schoeneberg@github.com>
2024-04-29 12:38:29 +0200

    Add a few descriptions for load functions

--- liquidcosmo/__init__.py
+++ liquidcosmo/__init__.py
@@ -2,15 +2,23 @@ from .matplotlib_defaults import initialize_plots
 initialize_plots()
 from .folder import folder
 from .foldercollection import foldercollection
+
+# Load a chain generally
 def load(*args,**kwargs):
   if len(args)==1 and isinstance(args[0],str):
     return folder.load(args[0],**kwargs)
   else:
     return foldercollection.load(*args,**kwargs)
+
+# Directly load chain only
 def load_chain(path,**kwargs):
   return folder.load(path,**kwargs).get_chain()
+
+# Load a file manually (providing all necessary keyword arguments)
 def load_manually(paths,**kwargs):
   return folder.load_manually(path,**kwargs)
+
+# Load a bestfit file from montepython as a single-item chain
 def loadbestfit(path,**kwargs):
   return folder.loadbestfit(path,**kwargs)
 #variational NN similar to GP
