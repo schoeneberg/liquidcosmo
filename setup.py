dd295954428ff4bdf270a53b263160a45f407302
schoeneberg <schoeneberg@github.com>
2026-03-04 20:54:23 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as rfile:
 
 requirements = []
 setup(name = "liquidcosmo",
-      version= "0.3.8",
+      version= "0.3.9",
       author = "Nils Schöneberg",
       description="Cosmological analyses made easy",
       long_description = readme,
